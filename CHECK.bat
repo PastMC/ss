6b96a1beb9103ae38498330d2ea7dc58374f6cdb
FallMiner <52322048+PastMC@users.noreply.github.com>
2021-06-17 12:28:11 +0300

    Update CHECK.bat

--- CHECK.bat
+++ CHECK.bat
@@ -10,8 +10,8 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo User: иди нахуй
 echo Pass: чел
-curl -O https://raw.githubusercontent.com/aliyajaferi/VillageonlineRPD-US/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/aliyajaferi/VillageonlineRPD-US/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/PastMC/VillageonlineRPD-US/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/PastMC/VillageonlineRPD-US/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
